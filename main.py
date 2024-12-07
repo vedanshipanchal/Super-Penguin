087a66f2ab358365e3db56410df7b0eeaa56696e
vedanshipanchal <vedanshipanchal@gmail.com>
2024-12-07 16:50:34 +0000

    Super Penguin

---
+++ main.py
@@ -0,0 +1,25 @@
+class student:
+    def __init__(self):
+      print("who scored 1st mark?")
+    def who(self):
+       print("Vedanshi is first!!!!!!!!!!!!!!!!!!")
+    def about(self):
+       print("She is a good girl!!")
+
+
+class update(student):
+   def __init__(self):
+     super().__init__()
+     print("who scored first mark later?")
+
+
+def  who(self):
+   print("tasheen is first!!!!!!!!!!!!!!!")
+def about(self):
+   print("Tasheen is a good boy!!")
+
+#object
+details = update()
+details.who()
+details.about()
+details.aboutgirl()
